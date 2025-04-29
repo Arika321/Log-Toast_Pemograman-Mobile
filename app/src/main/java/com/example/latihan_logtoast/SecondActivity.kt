62f2ddccbba109096fefc5be994bdce6a506bcc6
Arika321 <arika123azhar@gmail.com>
2025-04-29 20:12:00 +0700

    first commit

---
+++ app/src/main/java/com/example/latihan_logtoast/SecondActivity.kt
@@ -0,0 +1,24 @@
+package com.example.latihan_logtoast
+
+
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val textSecond : TextView =findViewById(R.id.txt_second)
+        val accepctText: String? = intent.getStringExtra("ini dikirim")
+        textSecond.text = accepctText
+
+    }
+}
